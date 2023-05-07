4e4ee24f65d867333c5f89f24549c6ea13822e6e
Filipe Teixeira <Filipe.Teixeira@effectory.com>
2023-05-07 16:39:35 +0100

    Added typing loop and improved navbar

--- js/longwave.js
+++ js/longwave.js
@@ -11,3 +11,12 @@ $grid.on('click', '.grid-item', function () {
     // trigger layout after item size changes
     $grid.masonry('layout');
 });
+
+var options = {
+    strings: ['This is an example of a typing effect', 'It can be used to create cool animated text', 'You can customize the speed, pauses, and more'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+};
+
+var typed = new Typed('.typing-effect', options);
