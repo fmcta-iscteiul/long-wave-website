0403555526c2bb88c3d7fdf8109b197037661de1
Filipe Teixeira <Filipe.Teixeira@effectory.com>
2023-08-27 13:35:32 +0100

    aestheatic fix

--- js/longwave.js
+++ js/longwave.js
@@ -1,25 +1,11 @@
-var $grid = $('.grid').masonry({
-    percentPosition: true,
-    columnWidth: '.grid-sizer',
-    itemSelector: '.grid-item',
-    stagger: 2
-});
-
-$grid.imagesLoaded().progress(function () {
-    $grid.masonry('layout');
-});
-
-// change size of item by toggling gigante class
-$grid.on('click', '.grid-item', function () {
-    $(this).toggleClass('gigante');
-    // trigger layout after item size changes
-    $grid.masonry('layout');
-});
-
 var typed = new Typed('#typed', {
-    strings: ['^1000 100% local', '^1000 100% sustainable', '^1000 100% cotton'],
+    strings: ['^1000 100% local', '^1000 100% low-impact', '^1000 100% ocean-conscious'],
     typeSpeed: 100,
     backSpeed: 75,
     smartBackspace: false,
     loop: true
 });
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
